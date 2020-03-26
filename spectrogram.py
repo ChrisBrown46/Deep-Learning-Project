36bf4596487827007edce0ff89a0adc79a8de992
ChrisBrown46 <christopher.r.brown@aggiemail.usu.edu>
2020-03-26 12:54:07 -0600

    Added inverse spectrogram

--- spectrogram.py
+++ spectrogram.py
@@ -1,20 +1,42 @@
 import librosa
 import numpy as np
+import pickle
 
 def scale_minmax(X, min=0.0, max=1.0):
+    """ Returns a normalized version of the input numpy array """
+    
     X_std = (X - X.min()) / (X.max() - X.min())
     X_scaled = X_std * (max - min) + min
     return X_scaled
 
-def wav_to_image(file_name):
-    # Load the data
-    import librosa
+def load_wav(file_name):
+    """ Returns a tuple of (sound, sr) """
+    
     y, sr = librosa.load(file_name)
-
-    # Trim silent edges
     sound, _ = librosa.effects.trim(y)
+    return sound, sr
 
-    # Construct the mel-log spectrogram
+def load_image(file_name):
+    """ Returns a tuple of (sound, sr) """
+    
+    with open(file_name, "rb") as fin:
+        return pickle.load(fin)
+
+def save_wav(file_name, sound, sr):
+    """ Librosa is deprecating in favor of PySoundFile  """
+    
+    librosa.output.write_wav(file_name, sound, sr)
+    
+def save_image(file_name, image, sr):
+    """ Pickles the data as a tuple of (image, sr) """
+    
+    with open(file_name, "wb+") as fout:
+        pickle.dump((image, sr), fout)
+
+def wav_to_image(sound, sr):
+    """ Converts (sound, sr) into a 3-D mel-log spectrogram """
+    
+    # Construct the mel spectrogram
     spectrogram = librosa.feature.melspectrogram(
         sound,
         sr=sr,
@@ -23,7 +45,8 @@ def wav_to_image(file_name):
         n_mels=128
     )
     
-    spectrogram = np.log(spectrogram + 1e-9)  # add small number to avoid log(0)
+    # Log the magnitude - add a small number to avoid log(0)
+    spectrogram = np.log(spectrogram + 1e-9)
 
     # Reshape to have 3 channels
     if len(spectrogram.shape) == 2:
@@ -35,8 +58,29 @@ def wav_to_image(file_name):
 
     return spectrogram
 
-def wav_to_pyplot_image(file_name):
-    image = wav_to_image(file_name)
+def image_to_wav(spectrogram, sr):
+    """ Converts (image, sr) into a 2-D audio numpy array """
+    
+    # Remove extra channels
+    spectrogram = spectrogram[:, :, 0]
+    
+    # Undo the log operation and remove the small number
+    spectrogram = np.exp(spectrogram) - 1e-9
+    
+    # Invert a mel power spectrogram to audio using Griffin-Lim
+    sound = librosa.feature.inverse.mel_to_audio(
+        spectrogram,
+        sr=sr,
+        n_fft=2048,
+        hop_length=512,
+    )
+    
+    return sound
+
+def wav_to_pyplot_image(sound, sr):
+    """ Converts (sound, sr) into a 3-D image for pyplot to render """
+    
+    image = wav_to_image(sound, sr)
     
     image = scale_minmax(image, 0, 255).astype(np.uint8)
     image = np.flip(image, axis=0)  # put low frequencies at the bottom in image
